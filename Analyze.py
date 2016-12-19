b5c6983953c16fe4e8b8044b7d12336bb7da71f8
尹健 <jian.yin@fenbeitong.com>
2016-12-19 17:01:29 +0800

    Analyze

---
+++ Analyze.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+from lxml import etree
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+f = open("index.html", "r")
+content = f.read()
+f.close()
+
+tree = etree.HTML(content.decode("utf-8"))
+
+
+hrefs = tree.xpath(u"//div[@class='item']")
+for href in hrefs:
+    image = href.xpath("a//img")
+    for img in image:
+        print str(img.attrib).decode("utf-8")
